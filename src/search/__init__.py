4bed747e5e4a238b0aeb48ba7042079b46d432ae
NikhilB19 <nban982@aucklanduni.ac.nz>
2023-10-06 11:24:06 +1300

    Added bing search.

--- src/search/__init__.py
+++ src/search/__init__.py
@@ -1,3 +1,4 @@
 from .searcher import Searcher
 from .news_searcher import News_searcher
 from .google_searcher import GoogleSearcher
+from .bing_searcher import BingSearcher
---
+++ src/search/bing_searcher.py
@@ -0,0 +1,88 @@
+import os
+import requests
+
+from searcher import Searcher
+from dotenv import load_dotenv
+
+class BingSearcher(Searcher):
+    
+    def general_search(self, query):
+        # Not filtered!!
+        # Web search
+        
+        response = requests.get(
+            "https://api.bing.microsoft.com/v7.0/search"
+            , headers = {'Ocp-Apim-Subscription-Key': os.getenv("BING_KEY")} #type: ignore
+            , params = {
+                'q': query
+                , 'mkt': 'en-US' # May change this later...
+            }
+        )
+        
+        response = response.json()['webPages']['value']
+    
+        response2return = [{k: v for k, v in d.items() if k in ['name','url','snippet']} for d in response]
+        
+        return response2return
+
+    def knowledge_graph_search(self, query):
+        
+        response = requests.get(
+            "https://api.bing.microsoft.com/v7.0/entities"
+            , headers = {'Ocp-Apim-Subscription-Key': os.getenv("BING_KEY")} #type: ignore
+            , params = {
+                'q': query
+                , 'mkt': 'en-US' # May change this later...
+            }
+        ).json()['entities']['value']
+        
+        entities = []
+        
+        if len(response) >= 1:
+            for entity in response:
+                entities.append({
+                    "Type": "Most likely match" if entity['entityPresentationInfo']['entityScenario'] == 'DominantEntity' else "Potential match"
+                    , "Name": entity['name']
+                    , "description": entity["description"]
+                })
+        
+        return entities
+
+        
+    def news_search(self, query):
+        # Only returns news descriptions :(
+        
+        
+        response = requests.get(
+            "https://api.bing.microsoft.com/v7.0/news/search"
+            , headers = {'Ocp-Apim-Subscription-Key': os.getenv("BING_KEY")} #type: ignore
+            , params = {
+                'q': query
+                , 'mkt': 'en-US' # May change this later...
+            }
+        )
+        
+        news = response.json()['value']
+        cleaned_news = []
+        
+        for article in news:
+            cleaned_news.append({
+                "source_name": article['provider'][0]['name']
+                , "headline": article['name']
+                , "description": article['description']
+                , "url": article['url']
+            })
+        
+        return cleaned_news
+        
+        
+if __name__ == "__main__":
+    load_dotenv()
+    
+    searcher = BingSearcher()
+
+    knowledge_graph_results = searcher.knowledge_graph_search("Donald Trump age")
+    general_results = searcher.general_search("Trump wants Postal Service to charge 'much more' for Amazon shipments.")
+    news_results = searcher.news_search("Trump wants Postal Service to charge 'much more' for Amazon shipments.")
+    
+    pass
